44fd34840c8df58bb114d6334753b4ff44e58759
digvijay67 <digvijaysingrajput67@gmail.com>
2025-10-20 19:04:14 +0530

    Updated xyz file

--- library_db.sql
+++ library_db.sql
@@ -1,10 +1,34 @@
-Create database library_db;
+CREATE DATABASE library_db;
 USE library_db;
 
-Create table book_issue (
-issue_id INT auto_increment Primary key,
-student_id varchar(10),
-student_name varchar(100),
-book_name varchar(150),
-issue_datatime timestamp default current_timestamp
+CREATE TABLE Student (
+    student_id INT AUTO_INCREMENT PRIMARY KEY,
+    student_name VARCHAR(100) NOT NULL,
+    email VARCHAR(50) UNIQUE,
+    phone VARCHAR(10)
 );
+
+CREATE TABLE Book (
+    book_id INT AUTO_INCREMENT PRIMARY KEY,
+    book_name VARCHAR(100) NOT NULL,
+    author VARCHAR(50) NOT NULL,
+    price DECIMAL(10,2)
+);
+
+CREATE TABLE Book_Issue (
+    issue_id INT AUTO_INCREMENT PRIMARY KEY,
+    student_id INT,
+    book_id INT,
+    issue_datetime TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (student_id) REFERENCES Student(student_id),
+    FOREIGN KEY (book_id) REFERENCES Book(book_id)
+);
+
+select * from Student;
+select * from Book_Issue;
+
+
+DROP TABLE IF EXISTS Book_Issue;
+DROP TABLE IF EXISTS Book;
+DROP TABLE IF EXISTS Student;
+
